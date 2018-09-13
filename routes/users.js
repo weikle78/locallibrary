4933405a88f4b33a5dcdc464e0a5a67a50e32ae5
Bryan Weikle <weikle78@ksu.edu>
2018-09-13 10:00:40 -0500

    with cool added

--- routes/users.js
+++ routes/users.js
@@ -5,5 +5,10 @@ var router = express.Router();
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
+/* GET users listing. */
+router.get('/cool', function(req, res, next) {
+  res.send('Your soooo cool');
+});
+
 
 module.exports = router;
