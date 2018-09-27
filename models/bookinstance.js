90b6a9f8251bdf3e177a4fd015aa703af4f31306
Bryan Weikle <weikle78@ksu.edu>
2018-09-27 10:48:35 -0500

    9_27_18 Added code for book instance Formatted date

--- models/bookinstance.js
+++ models/bookinstance.js
@@ -1,3 +1,5 @@
+var moment = require('moment');
+
 var mongoose = require('mongoose');
 
 var Schema = mongoose.Schema;
@@ -13,9 +15,10 @@ var BookInstanceSchema = new Schema(
 
 // Virtual for bookinstance's URL
 BookInstanceSchema
-.virtual('url')
+.virtual('due_back_formatted')
 .get(function () {
-  return '/catalog/bookinstance/' + this._id;
+  return moment(this.due_back).format('MMMM Do, YYYY');
+
 });
 
 //Export model
